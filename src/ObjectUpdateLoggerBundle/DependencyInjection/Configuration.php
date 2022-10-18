c137d9646e92ef9e8f778e4844180c1c984f1c73
Pawel Filipkowski <p.filipkowski@lemonmind.com>
2022-10-18 09:57:53 +0200

    remove unnecessary comments

--- src/ObjectUpdateLoggerBundle/DependencyInjection/Configuration.php
+++ src/ObjectUpdateLoggerBundle/DependencyInjection/Configuration.php
@@ -7,11 +7,6 @@ namespace Lemonmind\ObjectUpdateLoggerBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
-/**
- * This is the class that validates and merges configuration from your app/config files.
- *
- * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
- */
 class Configuration implements ConfigurationInterface
 {
     /**
@@ -20,10 +15,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder('lemonmind_object_update_logger');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
 
         return $treeBuilder;
     }
