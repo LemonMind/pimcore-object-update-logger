679e0be518c583c2a870b81d53264f577320b130
Pawel Filipkowski <p.filipkowski@lemonmind.com>
2022-10-18 15:29:24 +0200

    remove typo

--- src/ObjectUpdateLoggerBundle/DependencyInjection/Configuration.php
+++ src/ObjectUpdateLoggerBundle/DependencyInjection/Configuration.php
@@ -20,7 +20,6 @@ class Configuration implements ConfigurationInterface
             ->children()
                 ->scalarNode('classes_to_log')->end()
                 ->scalarNode('objects_to_log')->end()
-                ->scalarNode('disable_class_log')->end()
                 ->booleanNode('disable_class_log')->end()
                 ->booleanNode('disable_object_log')->end()
             ->end();
